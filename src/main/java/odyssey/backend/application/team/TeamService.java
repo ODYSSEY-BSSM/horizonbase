888c889bdc330831df2c778411235eaa46ed421c
leegunwoo <leegunwoo0325@gmail.com>
2025-09-22 00:54:16 +0900

    refactor(#191) : 검증 로직

--- src/main/java/odyssey/backend/application/team/TeamService.java
+++ src/main/java/odyssey/backend/application/team/TeamService.java
@@ -22,8 +22,9 @@ public class TeamService {
     }
 
     public void delete(Long id, User user){
-        Team team = teamRepository.findByLeader(user)
-                .orElseThrow(() -> new IllegalArgumentException("팀은 팀장만 삭제할 수 있읍니다."));
+        Team team = findByTeamId(id);
+
+        team.validateLeader(user);
 
         teamRepository.delete(team);
     }
