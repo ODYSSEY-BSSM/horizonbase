07e9bdb041c323c0d2c91d2778ade70ee6c1d636
hsm <hll2071@naver.com>
2025-09-22 17:39:27 +0900

    refactor(#192) : CookieUtil 수정
    RestAPI는 자동으로 브라우저 쿠키에 토큰을 담아 줄 수 있지만 웹소켓은 무조건 JS를 거쳐야 함, 이 때 httpOnly가 true면 막힘

--- src/main/java/odyssey/backend/infrastructure/cookie/CookieUtil.java
+++ src/main/java/odyssey/backend/infrastructure/cookie/CookieUtil.java
@@ -2,18 +2,22 @@ package odyssey.backend.infrastructure.cookie;
 
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CookieUtil {
 
     public void addCookie(HttpServletResponse response, String name, String value, int maxAge) {
-        Cookie cookie = new Cookie(name, value);
-        cookie.setHttpOnly(true);
-        cookie.setSecure(true);
-        cookie.setPath("/");
-        cookie.setMaxAge(maxAge);
-        response.addCookie(cookie);
+        ResponseCookie cookie = ResponseCookie.from(name, value)
+                .path("/")
+                .maxAge(maxAge)
+                .httpOnly(false)
+                .secure(true)
+                .sameSite("None")
+                .build();
+
+        response.addHeader("Set-Cookie", cookie.toString());
     }
 
 }
