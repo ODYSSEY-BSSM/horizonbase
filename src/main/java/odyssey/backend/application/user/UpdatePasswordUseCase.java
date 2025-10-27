0cf3c900e565f76e229d4f61faddafeed2c68fc6
leegunwoo <leegunwoo0325@gmail.com>
2025-10-27 11:38:16 +0900

    feat(#218) : 비밀번호 ㅂ녀경 로직 추가

---
+++ src/main/java/odyssey/backend/application/user/UpdatePasswordUseCase.java
@@ -0,0 +1,34 @@
+package odyssey.backend.application.user;
+
+import lombok.RequiredArgsConstructor;
+import odyssey.backend.domain.auth.UpdatePasswordVerification;
+import odyssey.backend.domain.auth.User;
+import odyssey.backend.infrastructure.persistence.auth.UpdatePasswordVerificationRepository;
+import odyssey.backend.infrastructure.persistence.auth.UserRepository;
+import odyssey.backend.presentation.user.dto.request.UpdatePasswordRequest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UpdatePasswordUseCase {
+
+    private final UpdatePasswordVerificationRepository updatePasswordVerificationRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+
+    @Transactional
+    public void updatePassword(User user, UpdatePasswordRequest request) {
+        user.updatePassword(passwordEncoder.encode(request.getPassword()));
+    }
+
+    private void validate(User user, UpdatePasswordRequest request) {
+        UpdatePasswordVerification updatePasswordVerification = updatePasswordVerificationRepository.findById(user.getEmail())
+                .orElseThrow(() -> new IllegalArgumentException("집가고싶다."));
+
+        updatePasswordVerification.validateVerified();
+
+        updatePasswordVerificationRepository.delete(updatePasswordVerification);
+    }
+}
