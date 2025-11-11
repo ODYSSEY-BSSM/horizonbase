0421d3d0dc4dd049752def0c121faea7b2ddcdae
leegunwoo <leegunwoo0325@gmail.com>
2025-11-11 13:46:01 +0900

    feat(#238) : 컨트롤러 작성

---
+++ src/main/java/odyssey/backend/presentation/csrf/CsrfController.java
@@ -0,0 +1,17 @@
+package odyssey.backend.presentation.csrf;
+
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/csrf")
+public class CsrfController {
+
+    @GetMapping
+    public CsrfToken getCsrfToken(CsrfToken token) {
+        return token;
+    }
+
+}
