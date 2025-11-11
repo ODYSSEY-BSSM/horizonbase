b2214bc878e1fe00af3e37532d4a9ffad828f8a7
leegunwoo <leegunwoo0325@gmail.com>
2025-11-11 12:08:03 +0900

    refactor(#234) : 파일구조 변경

--- src/test/java/odyssey/backend/roadmap/RoadmapControllerTest.java
+++ src/test/java/odyssey/backend/roadmap/RoadmapControllerTest.java
@@ -1,12 +1,12 @@
 package odyssey.backend.roadmap;
 
 import odyssey.backend.domain.auth.User;
-import odyssey.backend.domain.roadmap.Color;
 import odyssey.backend.domain.roadmap.Icon;
 import odyssey.backend.global.RestDocsSupport;
 import odyssey.backend.presentation.roadmap.dto.request.RoadmapRequest;
 import odyssey.backend.presentation.roadmap.dto.response.PersonalRoadmapResponse;
 import odyssey.backend.presentation.roadmap.dto.response.RoadmapCountResponse;
+import odyssey.backend.shared.color.Color;
 import odyssey.backend.shared.test.UserCreate;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
