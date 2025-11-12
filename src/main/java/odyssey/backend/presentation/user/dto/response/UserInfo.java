7e8ce3c2b09495e6d427ba055a7f112552ad2f95
leegunwoo <leegunwoo0325@gmail.com>
2025-11-13 04:42:14 +0900

    refactor(#264) : db 터지는거 방지

--- src/main/java/odyssey/backend/presentation/user/dto/response/UserInfo.java
+++ src/main/java/odyssey/backend/presentation/user/dto/response/UserInfo.java
@@ -1,20 +1,19 @@
 package odyssey.backend.presentation.user.dto.response;
 
-import odyssey.backend.domain.auth.Role;
 import odyssey.backend.domain.auth.User;
 
 public record UserInfo(
         Long uuid,
         String username,
         String email,
-        Role role
+        String role
 ) {
     public static UserInfo from(User user){
         return new UserInfo(
                 user.getUuid(),
                 user.getUsername(),
                 user.getEmail(),
-                user.getRole()
+                user.getRole().toString()
         );
     }
 }
