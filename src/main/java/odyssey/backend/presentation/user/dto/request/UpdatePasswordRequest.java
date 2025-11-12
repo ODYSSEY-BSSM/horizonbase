e08f172941f68df93036b022fc80ecd153a70935
leegunwoo <leegunwoo0325@gmail.com>
2025-11-13 02:18:22 +0900

    feat(#257) : 리퀘스트 필드에 이메일 추가

--- src/main/java/odyssey/backend/presentation/user/dto/request/UpdatePasswordRequest.java
+++ src/main/java/odyssey/backend/presentation/user/dto/request/UpdatePasswordRequest.java
@@ -1,5 +1,6 @@
 package odyssey.backend.presentation.user.dto.request;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -9,6 +10,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UpdatePasswordRequest {
+
+    @Email(message = "이메일 형식이어야합니다")
+    @NotEmpty(message = "필수값입니다.")
+    private String email;
 
     @NotEmpty(message = "필수값입니다.")
     private String password;
