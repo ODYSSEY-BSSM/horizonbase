235478ab59aab6aeab0d15069ec625d73e424074
leegunwoo <leegunwoo0325@gmail.com>
2025-11-14 00:38:55 +0900

    refactor(#281) : 필요없는 클래스 제거

--- src/main/java/odyssey/backend/presentation/ai/dto/response/GenerateResponse.java
+++
@@ -1,9 +0,0 @@
-package odyssey.backend.presentation.ai.dto.response;
-
-public record GenerateResponse(
-        String result
-) {
-    public static GenerateResponse from(String result){
-        return new GenerateResponse(result);
-    }
-}
