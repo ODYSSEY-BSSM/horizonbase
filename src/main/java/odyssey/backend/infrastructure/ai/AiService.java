36049c160532822011e5bb7459b11c5c3178b882
leegunwoo <leegunwoo0325@gmail.com>
2025-11-14 00:08:38 +0900

    refactor(#215) : 세팅

---
+++ src/main/java/odyssey/backend/infrastructure/ai/AiService.java
@@ -0,0 +1,14 @@
+package odyssey.backend.infrastructure.ai;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Service
+@RequiredArgsConstructor
+public class AiService {
+
+    private final WebClient webClient;
+
+    public String generate(Gene)
+}
