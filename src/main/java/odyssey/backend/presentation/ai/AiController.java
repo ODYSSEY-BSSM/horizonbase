42f87ccacc3ba2afd7d64496f4184a7916581096
leegunwoo <leegunwoo0325@gmail.com>
2025-11-14 00:39:04 +0900

    refactor(#281) : 컨트롤러 수정

--- src/main/java/odyssey/backend/presentation/ai/AiController.java
+++ src/main/java/odyssey/backend/presentation/ai/AiController.java
@@ -3,8 +3,10 @@ package odyssey.backend.presentation.ai;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import odyssey.backend.infrastructure.ai.AiService;
-import odyssey.backend.presentation.ai.dto.request.GenerateRequest;
-import odyssey.backend.presentation.ai.dto.response.GenerateResponse;
+import odyssey.backend.presentation.ai.dto.request.GenerateRoadmapRequest;
+import odyssey.backend.presentation.ai.dto.response.AiNodeListResponse;
+import odyssey.backend.shared.response.CommonResponse;
+import odyssey.backend.shared.response.SingleCommonResponse;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,9 +20,9 @@ public class AiController {
     private final AiService aiService;
 
     @PostMapping
-    public GenerateResponse generateAi(
-            @RequestBody @Valid GenerateRequest generateRequest) {
-        return aiService.generate(generateRequest);
+    public SingleCommonResponse<AiNodeListResponse> generateAiRoadmap(
+            @RequestBody @Valid GenerateRoadmapRequest generateRequest) {
+        return CommonResponse.ok(aiService.generate(generateRequest));
     }
 
 }
