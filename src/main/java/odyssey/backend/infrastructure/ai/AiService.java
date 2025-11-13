f8c219a1ecb1d3732f7fdc1abfb14a323df6976b
leegunwoo <leegunwoo0325@gmail.com>
2025-11-14 00:22:36 +0900

    feat#281) : 일단 서비스

--- src/main/java/odyssey/backend/infrastructure/ai/AiService.java
+++ src/main/java/odyssey/backend/infrastructure/ai/AiService.java
@@ -1,6 +1,9 @@
 package odyssey.backend.infrastructure.ai;
 
 import lombok.RequiredArgsConstructor;
+import odyssey.backend.presentation.ai.dto.request.GenerateRequest;
+import odyssey.backend.presentation.ai.dto.response.GenerateResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -10,5 +13,16 @@ public class AiService {
 
     private final WebClient webClient;
 
-    public String generate(Gene)
+    @Value("${ai.base-url}")
+    private String baseUrl;
+
+    public GenerateResponse generate(GenerateRequest request){
+        return webClient.post()
+                .uri(baseUrl + "/generate")
+                .bodyValue(request)
+                .retrieve()
+                .bodyToMono(GenerateResponse.class)
+                .block();
+    }
+
 }
