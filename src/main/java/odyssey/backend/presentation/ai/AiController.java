9bae11fcfd06f25109530c1fe83eff9089c24b8b
leegunwoo <leegunwoo0325@gmail.com>
2025-11-16 23:44:30 +0900

    feat(#286) : 작성

--- src/main/java/odyssey/backend/presentation/ai/AiController.java
+++ src/main/java/odyssey/backend/presentation/ai/AiController.java
@@ -3,14 +3,15 @@ package odyssey.backend.presentation.ai;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import odyssey.backend.infrastructure.ai.AiService;
+import odyssey.backend.presentation.ai.dto.request.GenerateQuizRequest;
 import odyssey.backend.presentation.ai.dto.request.GenerateRoadmapRequest;
+import odyssey.backend.presentation.ai.dto.request.ModifyNodeRequest;
 import odyssey.backend.presentation.ai.dto.response.AiNodeListResponse;
+import odyssey.backend.presentation.ai.dto.response.ModifyNodeResponse;
+import odyssey.backend.presentation.ai.dto.response.QuizResponse;
 import odyssey.backend.shared.response.CommonResponse;
 import odyssey.backend.shared.response.SingleCommonResponse;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -19,10 +20,25 @@ public class AiController {
 
     private final AiService aiService;
 
-    @PostMapping
+    @PostMapping("/roadmaps")
     public SingleCommonResponse<AiNodeListResponse> generateAiRoadmap(
-            @RequestBody @Valid GenerateRoadmapRequest generateRequest) {
-        return CommonResponse.ok(aiService.generate(generateRequest));
+            @RequestBody @Valid GenerateRoadmapRequest request
+    ) {
+        return CommonResponse.ok(aiService.generateRoadmap(request));
+    }
+
+    @PatchMapping("/nodes")
+    public SingleCommonResponse<ModifyNodeResponse> updateAiNodes(
+            @RequestBody @Valid ModifyNodeRequest request
+    ){
+        return CommonResponse.ok(aiService.modifyNode(request));
+    }
+
+    @PostMapping("/quizs")
+    public SingleCommonResponse<QuizResponse> generateAiQuiz(
+            @RequestBody @Valid GenerateQuizRequest request
+    ){
+        return CommonResponse.ok(aiService.generateQuiz(request));
     }
 
 }
