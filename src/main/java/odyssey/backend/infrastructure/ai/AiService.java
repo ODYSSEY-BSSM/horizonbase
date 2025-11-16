f5d17e6c655f7fc006b0a9d4e406953186617c0e
leegunwoo <leegunwoo0325@gmail.com>
2025-11-16 22:08:20 +0900

    feat(#286) : 예외처리

--- src/main/java/odyssey/backend/infrastructure/ai/AiService.java
+++ src/main/java/odyssey/backend/infrastructure/ai/AiService.java
@@ -1,8 +1,11 @@
 package odyssey.backend.infrastructure.ai;
 
 import lombok.RequiredArgsConstructor;
+import odyssey.backend.infrastructure.ai.exception.FailedAiResponseException;
 import odyssey.backend.presentation.ai.dto.request.GenerateRoadmapRequest;
+import odyssey.backend.presentation.ai.dto.request.ModifyNodeRequest;
 import odyssey.backend.presentation.ai.dto.response.AiNodeListResponse;
+import odyssey.backend.presentation.ai.dto.response.ModifyNodeResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -17,12 +20,29 @@ public class AiService {
     private String baseUrl;
 
     public AiNodeListResponse generate(GenerateRoadmapRequest request){
-        return webClient.post()
-                .uri(baseUrl + "/create-roadmap")
-                .bodyValue(request)
-                .retrieve()
-                .bodyToMono(AiNodeListResponse.class)
-                .block();
+        try {
+            return webClient.post()
+                    .uri(baseUrl + "/create-roadmap")
+                    .bodyValue(request)
+                    .retrieve()
+                    .bodyToMono(AiNodeListResponse.class)
+                    .block();
+        }catch (Exception e){
+            throw new FailedAiResponseException();
+        }
+    }
+
+    public ModifyNodeResponse modifyNode(ModifyNodeRequest request){
+        try {
+            return webClient.post()
+                    .uri(baseUrl + "/create-roadmap")
+                    .bodyValue(request)
+                    .retrieve()
+                    .bodyToMono(ModifyNodeResponse.class)
+                    .block();
+        }catch (Exception e){
+            throw new FailedAiResponseException();
+        }
     }
 
 }
