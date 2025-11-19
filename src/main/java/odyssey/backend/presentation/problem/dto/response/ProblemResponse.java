05b04cc86b93dd9c8b281213f7269f03523bf6c2
leegunwoo <leegunwoo0325@gmail.com>
2025-11-20 07:41:44 +0900

    feat(#314) : 내가 이겼다

--- src/main/java/odyssey/backend/presentation/problem/dto/response/ProblemResponse.java
+++ src/main/java/odyssey/backend/presentation/problem/dto/response/ProblemResponse.java
@@ -3,15 +3,19 @@ package odyssey.backend.presentation.problem.dto.response;
 import odyssey.backend.domain.problem.Problem;
 import odyssey.backend.domain.problem.Status;
 
+import java.util.List;
+
 public record ProblemResponse(
         Long id,
         String title,
+        List<String> choices,
         Status status
 ) {
     public static ProblemResponse from(Problem problem) {
         return new ProblemResponse(
                 problem.getId(),
                 problem.getTitle(),
+                problem.getChoices(),
                 problem.getStatus()
         );
     }
