797b8a3269304410eda0153797132e7ad9e33572
hsm <hll2071@naver.com>
2025-11-25 14:39:49 +0900

    feat(#352) : 텍스트 메세징
    텍스트 메세징

--- src/main/java/odyssey/backend/application/text/DeleteTextUseCase.java
+++ src/main/java/odyssey/backend/application/text/DeleteTextUseCase.java
@@ -1,7 +1,10 @@
 package odyssey.backend.application.text;
 
 import lombok.RequiredArgsConstructor;
+import odyssey.backend.domain.text.Text;
+import odyssey.backend.domain.text.exception.TextNotFoundException;
 import odyssey.backend.infrastructure.persistence.text.TextRepository;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -9,9 +12,19 @@ import org.springframework.stereotype.Service;
 public class DeleteTextUseCase {
 
     private final TextRepository textRepository;
+    private final SimpMessagingTemplate messagingTemplate;
 
     public void deleteText(Long textId) {
+        Text text = textRepository.findById(textId)
+                .orElseThrow(TextNotFoundException::new);
+
+        Long roadmapId = text.getRoadmapId();
+
         textRepository.deleteById(textId);
+
+        if (text.getRoadmap().getTeam() != null) {
+            messagingTemplate.convertAndSend("/topic/text/roadmap/" + roadmapId + "/deleted", textId);
+        }
     }
 
 }
