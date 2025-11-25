70dd1f7471a5febaeec4c0b26cce467bd2f62fba
hsm <hll2071@naver.com>
2025-11-25 13:49:46 +0900

    feat(#352) : 섹션 메세징
    섹션 메세징

--- src/main/java/odyssey/backend/application/section/CreateSectionUseCase.java
+++ src/main/java/odyssey/backend/application/section/CreateSectionUseCase.java
@@ -8,6 +8,7 @@ import odyssey.backend.infrastructure.persistence.roadmap.RoadmapRepository;
 import odyssey.backend.infrastructure.persistence.section.SectionRepository;
 import odyssey.backend.presentation.section.dto.request.SectionRequest;
 import odyssey.backend.presentation.section.dto.response.SectionResponse;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -16,16 +17,19 @@ public class CreateSectionUseCase {
 
     private final SectionRepository sectionRepository;
     private final RoadmapRepository roadmapRepository;
+    private final SimpMessagingTemplate messagingTemplate;
 
     public SectionResponse createSection(SectionRequest request, Long roadmapId){
         Roadmap roadmap = roadmapRepository.findById(roadmapId)
                 .orElseThrow(RoadmapNotFoundException::new);
 
-        return SectionResponse.from(
-                sectionRepository.save(
-                        Section.from(request, roadmap)
-                )
-        );
+        SectionResponse response = SectionResponse.from(sectionRepository.save(Section.from(request, roadmap)));
+
+        if (roadmap.getTeam() == null) {
+            messagingTemplate.convertAndSend("/topic/section/roadmap/" + roadmapId + "/created", response);
+        }
+
+        return response;
     }
 
 }
--- src/main/java/odyssey/backend/application/section/DeleteSectionUseCase.java
+++ src/main/java/odyssey/backend/application/section/DeleteSectionUseCase.java
@@ -1,7 +1,10 @@
 package odyssey.backend.application.section;
 
 import lombok.RequiredArgsConstructor;
+import odyssey.backend.domain.section.Section;
+import odyssey.backend.domain.section.exception.SectionNotFoundException;
 import odyssey.backend.infrastructure.persistence.section.SectionRepository;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -9,9 +12,17 @@ import org.springframework.stereotype.Service;
 public class DeleteSectionUseCase {
 
     private final SectionRepository sectionRepository;
+    private final SimpMessagingTemplate messagingTemplate;
 
     public void deleteSection(Long sectionId) {
+        Section section = sectionRepository.findById(sectionId)
+                .orElseThrow(SectionNotFoundException::new);
+
+        Long roadmapId = section.getRoadmap().getId();
+
         sectionRepository.deleteById(sectionId);
+
+        messagingTemplate.convertAndSend("/topic/section/roadmap/" + roadmapId + "/deleted", sectionId);
     }
 
 }
