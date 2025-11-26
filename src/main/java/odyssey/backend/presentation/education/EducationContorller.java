eb3c1874837562877b49406f7448cad12a12bf38
leegunwoo <leegunwoo0325@gmail.com>
2025-11-27 07:25:31 +0900

    refactor : 수정

--- src/main/java/odyssey/backend/presentation/education/EducationContorller.java
+++ src/main/java/odyssey/backend/presentation/education/EducationContorller.java
@@ -14,10 +14,10 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@ResponseStatus
+@RestController
 @RequestMapping("/education")
 public class EducationContorller {
 
